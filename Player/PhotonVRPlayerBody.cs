1bcc47071d8ae452196229cfa56d57ea73fd1de0
MaxNiftyNine <83783498+MaxNiftyNine@users.noreply.github.com>
2023-08-23 13:03:31 -0400

    Update PhotonVRPlayerBody.cs

--- Player/PhotonVRPlayerBody.cs
+++ Player/PhotonVRPlayerBody.cs
@@ -5,14 +5,9 @@ using UnityEngine;
 public class PhotonVRPlayerBody : MonoBehaviour
 {
     public Transform Head;
-    [Tooltip("Enable this if you want the body to line up with the default gorilla locomotion hitbox")]
-    public bool GorillaLocomotion;
 
     private void Start() {
         transform.parent = Head;
-        if (GorillaLocomotion){
-            transform.localScale = new Vector3(0,-0.24f,-0.06f);
-        }
     }
 
     private void Update()
